dd7ad63a6377014ba4f4c2447a72ac72e7a73416
James Sansbury <james@lullabot.com>
2023-08-04 14:49:35 -0400

    workaround task limitation of 100 targets

--- scripts/bakefile.sh
+++ scripts/bakefile.sh
@@ -18,7 +18,12 @@ for dockerfile in images/*/*/Dockerfile; do
 
     # If we are not overwriting and the tar already exists and has a size
     # greater than zero, continue to the next Dockerfile.
-    test "$OVERWRITE" = "false" && test -s "$dest" && echo "skipping $name" && continue
+    echo $OVERWRITE_EXISTING
+    echo $dest
+    if [[ "$OVERWRITE_EXISTING" != "true" ]] && [[ "$OVERWRITE_EXISTING" != "1" ]] && [[ -s "$dest" ]]; then
+        echo "Skipping $name; set OVERWRITE_EXISTING to true to overwrite this image" >&2
+        continue
+    fi
 
     obj=$(printf '{
         "context": "%s",
@@ -30,4 +35,3 @@ for dockerfile in images/*/*/Dockerfile; do
     jq --arg name "$name" --argjson obj "$obj" '.target += { ($name): $obj } | .group.default.targets += [$name]' bake.json > newbake.json
     rm bake.json && mv newbake.json bake.json
 done
-rm -f newbake.json
--- scripts/build.sh
+++ scripts/build.sh
@@ -2,44 +2,45 @@
 
 set -e
 
-tarball=$1
 platform=${2-linux/amd64}
+parent=images/"$1"
 
-# Ensure the tarball exists and is larger than zero bytes.
-test -s "$tarball"
+for tarball in "$parent"/*/image.tar; do
+    # Ensure the tarball exists and is larger than zero bytes.
+    test -s "$tarball"
 
-dir=$(dirname "$tarball")
-parent=$(dirname "$dir")
-name=$(cat "$dir/NAME")
-tag=$(cut -d' ' -f1 < "$dir/TAGS")
-# shellcheck disable=SC2207
-aliases=($(cut -d' ' -f2- < "$dir/TAGS"))
-image=${NAMESPACE}/${name}:${tag}
+    dir=$(dirname "$tarball")
+    name=$(cat "$dir/NAME")
+    tag=$(cut -d' ' -f1 < "$dir/TAGS")
+    # shellcheck disable=SC2207
+    aliases=($(cut -d' ' -f2- < "$dir/TAGS"))
+    image=${NAMESPACE}/${name}:${tag}
 
-if grep -q "^$image\$" "$parent/built" 2>/dev/null && [[ "$OVERWRITE" != "true" ]]; then
-    echo "Skipping $image; set OVERWRITE to true to overwrite this image"
-    exit
-fi
+    if grep -q "^$image\$" "$parent/built" 2>/dev/null && [[ "$OVERWRITE_EXISTING" != "true" ]] && [[ "$OVERWRITE_EXISTING" != "1" ]]; then
+        echo "Skipping $image; set OVERWRITE_EXISTING to true to overwrite this image" >&2
+        continue
+    fi
 
-temp=$(mktemp -d)
-trap 'rm -rf $temp' EXIT SIGINT
+    temp=$(mktemp -d)
+    trap 'rm -rf $temp' EXIT SIGINT
 
-# Extract the tarball to the temp directory.
-tar -C "$temp" -xf "$tarball"
+    # Extract the tarball to the temp directory.
+    tar -C "$temp" -xf "$tarball"
 
-# Remove volumes and entrypoint from the manifest.
-config=$(jq -r '.[0].Config' "${temp}/manifest.json")
-jq 'del(.config.Volumes,.config.Entrypoint)' "${temp}/${config}" > "${temp}/config.json"
-mv -f "${temp}/config.json" "${temp}/${config}"
-tar -C "$temp" -c . | docker image import --platform "$platform" - "$image"
+    # Remove volumes and entrypoint from the manifest.
+    config=$(jq -r '.[0].Config' "${temp}/manifest.json")
+    jq 'del(.config.Volumes,.config.Entrypoint)' "${temp}/${config}" > "${temp}/config.json"
+    mv -f "${temp}/config.json" "${temp}/${config}"
+    tar -C "$temp" -c . | docker image import --platform "$platform" - "$image"
 
-echo "$image" >> "$parent/built"
+    echo "$image" >> "$parent/built"
 
-for alias in "${aliases[@]}"; do
-    alias_tag="${NAMESPACE}/${name}:${alias}"
-    docker tag "$image" "$alias_tag"
-    echo "$alias_tag" >> "$parent/built"
+    for alias in "${aliases[@]}"; do
+        alias_tag="${NAMESPACE}/${name}:${alias}"
+        docker tag "$image" "$alias_tag"
+        echo "$alias_tag" >> "$parent/built"
+    done
+
+    # If we've gotten this far, it's safe to delete the tarball.
+    rm "$tarball"
 done
-
-# If we've gotten this far, it's safe to delete the tarball.
-rm "$tarball"

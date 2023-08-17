653e15de1a20217c67dc7f7addcdb76cb3fd8fdd
James Sansbury <james@lullabot.com>
2023-08-16 21:42:56 -0400

    cleanup

--- scripts/push.sh
+++ scripts/push.sh
@@ -9,5 +9,10 @@ fi
 
 image=$1
 
+if [[ -f "images/$image/pushed" ]] && [[ "$OVERWRITE_EXISTING" != "true" ]] && [[ "$OVERWRITE_EXISTING" != "1" ]]; then
+    echo "Skipping $image; rm images/$image/pushed or set OVERWRITE_EXISTING to true to rebuild this image" >&2
+    exit
+fi
+
 docker push --all-tags "$NAMESPACE/$image"
 touch "images/$image/pushed"
